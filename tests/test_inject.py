b3dcdb3802a30615baab48f8c0845fee4b57d437
Hide <padocon@naver.com>
2019-11-12 22:34:36 +0900

    Implement inject test codes

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -1,9 +1,35 @@
-from pythondi import inject
+from pythondi import inject, Provider, configure_after_clear
 
 
-def test_inject_with_parameter():
-    pass
+class Repo:
+    def __init__(self):
+        pass
+
+
+class SQLRepo:
+    def __init__(self):
+        pass
 
 
 def test_inject_without_parameter():
-    pass
+    provider = Provider()
+    provider.bind(Repo, SQLRepo)
+    configure_after_clear(provider)
+
+    @inject()
+    def func(repo: Repo):
+        assert isinstance(repo, SQLRepo)
+
+    func()
+
+
+def test_inject_with_parameter():
+    provider = Provider()
+    provider.bind(Repo, SQLRepo)
+    configure_after_clear(provider)
+
+    @inject(repo=SQLRepo)
+    def func(repo):
+        assert isinstance(repo, SQLRepo)
+
+    func()
